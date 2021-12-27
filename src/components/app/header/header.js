857bca04962526a2e2f2a145e0736d632683a317
Kirill Sakun <kirillsakun03@gmail.com>
2021-12-27 10:35:49 +0300

    posthtml include added, some minor changes

---
+++ src/components/app/header/header.js
@@ -0,0 +1,5 @@
+import './header.scss';
+
+const root = document.querySelector('.app-header');
+// test alert init
+root.querySelector('button').addEventListener('click', () => { alert('Hello word!'); });
