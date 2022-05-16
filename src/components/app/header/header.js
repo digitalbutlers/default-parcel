9cc3f827b0438e819ef33c784f379293b0511850
kirillsakun <kirillsakun03@gmail.com>
2022-05-16 19:23:06 +0400

    update(components): minor js example update

--- src/components/app/header/header.js
+++ src/components/app/header/header.js
@@ -1,5 +1,9 @@
 import './header.scss';
 
-const root = document.querySelector('.app-header');
-// test alert init
-root.querySelector('button').addEventListener('click', () => { alert('Hello word!'); });
+// scope to avoid conflicts
+{
+	// use root as you usually use document
+	const root = document.querySelector('.app-header');
+	// eslint-disable-next-line no-console
+	console.log(root);
+}
